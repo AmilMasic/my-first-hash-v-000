1b3836e0fae64e7dfeb0f29d5ed1900c26abc874
Amil Masic <amil.masic@gmail.com>
2018-09-14 00:22:48 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  pet = {name => "Pink", species => "feline"}
+  pet = {"name" => "Pink", "species" => "feline"}
 
 end
 
